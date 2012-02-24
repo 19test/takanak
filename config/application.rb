e0978189b83d43528076caa1fe5e11e235d63081
Fatih Erikli <fatiherikli@gmail.com>
2012-02-24 12:38:12 +0200

    precompile

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Debitmanager
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+config.assets.precompile += ['960.css', 'style.css', 'custom.css']
+
   end
 end
