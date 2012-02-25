b3c169dead004410bc2e01d4b48bac74b0cc68e3
Fatih Erikli <fatiherikli@gmail.com>
2012-02-25 20:50:45 +0200

    validates

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -5,6 +5,7 @@ STATUS_CHOICES = {"paid"=>"Ödendi.", "not_paid"=>"Ödenmedi."}
 class Debit < ActiveRecord::Base
   belongs_to :friend
   validates_inclusion_of :status, :in => STATUS_CHOICES.keys
+  validates_numericality_of :amount, :presence => true, :message => '^Numerik bir değer giriniz.'
 
   def get_status
     STATUS_CHOICES[status]
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,8 +1,13 @@
+# -*- coding:utf-8 -*-
 class Friend < ActiveRecord::Base
   belongs_to  :user
   has_many  :debits
-
   accepts_nested_attributes_for :debits
+  validates_presence_of :name, :message => '^İsim alanı gereklidir.'
+  validates_presence_of :email, :message => '^Email alanı gereklidir.'
+  validates_format_of     :email,
+                          :with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                          :message    => '^Geçerli bir email giriniz.'
 
   def total_not_paid
       Debit.sum(:amount, :group=>:friend_id, :conditions=>["status='not_paid'"])[id] or 0
